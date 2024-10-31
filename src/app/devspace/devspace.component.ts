5800d1bc221255a05db0198aeab1324c3e14fb0d
Ufuk Özsahin <oezsahin95@icloud.com>
2024-10-31 12:58:48 +0100

    first commit

---
+++ src/app/devspace/devspace.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-devspace',
+  standalone: true,
+  imports: [],
+  templateUrl: './devspace.component.html',
+  styleUrl: './devspace.component.scss'
+})
+export class DevspaceComponent {
+
+}
