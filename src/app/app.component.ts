1f1c7d333782273d6776713cac470a2ef47051d0
Ufuk Özsahin <oezsahin95@icloud.com>
2024-11-04 16:38:42 +0100

    new channel component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DevspaceComponent } from './devspace/devspace.component';
+import { NewChannelComponent } from './new-channel/new-channel.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DevspaceComponent],
+  imports: [RouterOutlet, DevspaceComponent, NewChannelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'dabubble';
+
+  isNewChannelClicked: boolean = false;
+
+  handleNewChannelClicked(event: boolean) {
+    this.isNewChannelClicked = event;
+  }
+
+  handleNewChannelClosed() {
+    this.isNewChannelClicked = false;
+  }
 }
